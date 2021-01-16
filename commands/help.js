f856a524b897d54b51f11b5dd3647b11220ddcc5
John William Bowen <johnwb1776@gmail.com>
2021-01-15 20:59:13 -0500

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -8,15 +8,33 @@ module.exports = {
      */
 	execute(message, args) {
         let reply = new MessageEmbed()
-        .setAuthor('The Blue Alliance','https://frcdesigns.files.wordpress.com/2017/06/android_launcher_icon_blue_512.png')
-        .setColor('#3F51B5');
-        if (args.length == 0) {
+            .setAuthor('The Blue Alliance','https://frcdesigns.files.wordpress.com/2017/06/android_launcher_icon_blue_512.png')
+            .setColor('#3F51B5');
+        console.log(args[0]);
+        switch (args[0]) {
+            case 'status':
+                reply = reply
+                .setTitle('Help Status')
+                .addFields(
+                    {name: 'Syntax', value: '```tba Status ```',inline: false},
+                );
+                break;
+            case 'team':
+                reply = reply
+                .setTitle('team')
+                .addFields(
+                    {name: 'Syntax', value: '```tba team <team number>```',inline: false},
+                    {name: 'Exsample', value: '```tba team 540```',inline: false},
+                );
+                break;   
+            default:
             reply = reply
                 .setTitle('Help')
                 .addFields(
                     {name: 'Syntax', value: '```tba help <options>```',inline: false},
-                    {name:'Options', value: '```\nhelp\nstatus\nteam```',inline: false},
-                )
+                    {name:'Options', value: '```\nstatus\nteam```',inline: false},
+                );
+            break;
         }
         message.reply(null, reply);
     },
